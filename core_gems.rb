d342654aa7bb4eb85a672049492bda071b592e9e
sam <Sam Pointer>
2017-06-23 15:50:42 +0100

    remove some old core gems and let the fluentd gemspec pull them in

--- core_gems.rb
+++ core_gems.rb
@@ -1,7 +1,5 @@
 dir 'core_gems'
 download "bundler", "1.10.6"
-download "json", "1.8.1"
-download "msgpack", "0.5.11"
 download "cool.io", "1.4.2"
 download "http_parser.rb", "0.6.0"
 download "yajl-ruby", "1.2.1"
