e5ccd5edcaf6cc79a4def36d805d78ccac1bebda
sam <Sam Pointer>
2017-06-23 17:39:01 +0100

    bump version

--- config/projects/td-agent2.rb
+++ config/projects/td-agent2.rb
@@ -9,7 +9,7 @@ description "Treasure Agent: A data collector for Treasure Data"
 
 install_dir     "/opt/td-agent"
 build_version   "0.0.0"
-build_iteration 22
+build_iteration 23
 
 # creates required build directories
 dependency "preparation"
